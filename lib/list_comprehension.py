6533090bea9b37010e4799d4e87d0ca8fa05c786
jemojeremmy2@gmail.com <jemojeremmy2>
2024-05-20 17:58:27 +0300

    Implemented list comprehension functions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
+# lib/list_comprehension.py
 
 def return_evens(num_list):
-    pass
+    # Using list comprehension to filter even numbers
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    # Using list comprehension to add exclamation marks to each sentence
+    return [sentence + "!" for sentence in sentence_list]
